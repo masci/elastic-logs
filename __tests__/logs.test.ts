68ac8933b790b5edab4a4d3222d7d816ea0d42d6
Massimiliano Pippi <mpippi@gmail.com>
2021-09-30 11:27:03 +0200

    add logs conversion using ECS and pino

---
+++ __tests__/logs.test.ts
@@ -0,0 +1,28 @@
+import {run} from '../src/run'
+import {convert} from '../src/logs'
+import {anything, spy, when, verify, capture} from 'ts-mockito'
+import {readFileSync, rmSync} from 'fs'
+import {mkdtemp} from 'fs/promises'
+import * as os from 'os'
+import * as path from 'path'
+
+describe('Test logs module', () => {
+  beforeEach(() => {})
+
+  afterEach(() => {})
+
+  test('test conversion', async () => {
+    const tmpDir = await mkdtemp(path.join(os.tmpdir(), 'elastic-logs-'))
+    const inFile = './__tests__/logs.txt'
+    const outFile = path.join(tmpDir, 'out.log')
+    // load lines from file
+    let lines: string[] = String(readFileSync(inFile)).split('\n')
+    // convert lines into ECS, write to file
+    convert(lines, outFile)
+    // assert
+    let result: string[] = String(readFileSync(outFile)).split('\n')
+    expect(result.length).toBe(lines.length + 1) // count the trailing blank line in the log file
+    // cleanup
+    rmSync(tmpDir, {recursive: true, force: true})
+  })
+})
--- src/logs.ts
+++ src/logs.ts
@@ -1,1 +1,10 @@
-// stub
+import {default as Pino} from 'pino'
+import {default as ecs} from '@elastic/ecs-pino-format'
+
+export async function convert(lines: string[], path: string): Promise<void> {
+  const log = Pino(ecs(), Pino.destination(path))
+
+  for (const l of lines) {
+    log.info(l)
+  }
+}
