24d3a994b5dd79ae40ce01e7ec110c3d565a0295
Massimiliano Pippi <mpippi@gmail.com>
2021-09-30 18:16:39 +0200

    add elastic options + tests

--- __tests__/run.test.ts
+++ __tests__/run.test.ts
@@ -3,24 +3,36 @@ import {run} from '../src/run'
 import * as gh from '../src/github'
 import {anything, spy, when, verify, capture} from 'ts-mockito'
 
+const spiedGH = spy(gh)
+
 describe('Test config params', () => {
   beforeEach(() => {
     process.env['INPUT_REPO-TOKEN'] = 'qwerty'
+    when(
+      spiedGH.fetchJobs(anything(), anything(), anything(), anything())
+    ).thenResolve([]) // resolve with an empty array to avoid the subsequent API call
   })
 
   afterEach(() => {})
 
   test('Process job-names param', async () => {
+    // configure
     process.env['INPUT_JOB-NAMES'] = 'foo, bar, baz, foo bar baz'
-
-    const spiedGH = spy(gh)
-    when(
-      spiedGH.fetchJobs(anything(), anything(), anything(), anything())
-    ).thenResolve([]) // resolve with an empty array to avoid the subsequent API call
-
+    // run
     await run()
-
+    // assert
     const [, , , allowList] = capture(spiedGH.fetchJobs).last()
     expect(allowList).toEqual(['foo', 'bar', 'baz', 'foo bar baz'])
+  })
+
+  test('Missing Elastic config params', async () => {
+    // run
+    try {
+      await run()
+    } catch (e) {
+      expect(String(e)).toBe(
+        'invalid configuration: please set either cloud-id or addresses'
+      )
+    }
   })
 })
--- src/run.ts
+++ src/run.ts
@@ -3,20 +3,49 @@ import * as gh from './github'
 import * as process from 'process'
 import {HttpClient} from '@actions/http-client'
 
+const defaultIndex = 'foo'
+
+// Split comma separated inputs into an array of trimmed values
+export function getCommaSeparatedInput(value: string): string[] {
+  let retVal: string[] = []
+  if (value !== '') {
+    retVal = value.split(',')
+    // trim array items
+    retVal = retVal.map(s => s.trim())
+  }
+
+  return retVal
+}
+
 export async function run(): Promise<void> {
   try {
     // retrieve config params
+
+    // Github repo token
     const repoToken: string = core.getInput('repo-token', {required: true})
+    // List of jobs to collect logs from (all jobs when empty)
     const jobNames: string = core.getInput('job-names', {required: false}) || ''
-    let allowList: string[] = []
-    if (jobNames !== '') {
-      allowList = jobNames.split(',')
-      // trim job names
-      allowList = allowList.map(s => s.trim())
+    const allowList = getCommaSeparatedInput(jobNames)
+    // Elastic Cloud ID
+    const cloudId: string = core.getInput('cloud-id', {required: false}) || ''
+    // Elasticsearch addresses (when Cloud ID is not present)
+    const addrValue: string =
+      core.getInput('addresses', {required: false}) || ''
+    const addresses = getCommaSeparatedInput(addrValue)
+    // Elasticsearch index
+    const indexName: string =
+      core.getInput('index-name', {required: false}) || defaultIndex
+
+    // Ensure either Cloud ID or ES addresses are set
+    if (cloudId === '' && addresses.length === 0) {
+      throw new Error(
+        'invalid configuration: please set either cloud-id or addresses'
+      )
     }
 
     // get an authenticated HTTP client for the GitHub API
     const client: HttpClient = gh.getClient(repoToken)
+
     // get all the jobs for the current workflow
     const workflowId: string = process.env['GITHUB_RUN_ID'] || ''
     const repo: string = process.env['GITHUB_REPOSITORY'] || ''
@@ -27,6 +56,7 @@ export async function run(): Promise<void> {
       workflowId,
       allowList
     )
+
     // get the logs
     core.debug(`Getting logs for ${jobs.length} jobs`)
     for (const j of jobs) {
